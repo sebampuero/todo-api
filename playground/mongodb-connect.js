939e59d9c7bcc91d7cc5d2363cd2af06c07d264f
Sebastian <sebastian.ampuerom@gmail.com>
2017-06-24 12:49:23 +0200

    Initial commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,30 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //the same as above
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  // db.collection('Todos').insertOne({
+  //   text : 'Something to do',
+  //   completed : false
+  // },(err,result)=>{
+  //   if(err)
+  //     return console.log('Unable to insert todo',err);
+  //   console.log(JSON.stringify(result.ops,undefined,2));
+  // })
+  // db.collection('Users').insertOne({
+  //   name: 'Juan',
+  //   age : 14,
+  //   location : 'Germany'
+  // },(err,results)=>{
+  //   if(err)
+  //     return console.log('Unable to insert user',err);
+  //   console.log(JSON.stringify(results.ops[0]._id.getTimestamp(),undefined,2));
+  // })
+  db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); //the same as above
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
+  if(err){
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('594e3c0ffa4db44361698847')
+  // }).toArray().then((docs)=>{
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs,undefined,2));
+  // },(err)=>{
+  //   console.log('Unable to fetch todos',err);
+  // });
+  // db.collection('Todos').find().count().then((count)=>{
+  //   console.log(`Todos count: ${count}`)
+  // },(err)=>{
+  //   console.log('Unable to fetch todos',err);
+  // });
+  db.collection('Users').find({name:'Juan'}).toArray().then((docs)=>{
+    console.log('Users')
+    console.log(JSON.stringify(docs,undefined,1));
+  },(err)=>{
+    console.log('Error',err);
+  });
+
+  // db.close();
+});
